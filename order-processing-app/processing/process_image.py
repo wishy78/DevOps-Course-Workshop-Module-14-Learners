506dadf448e7772e951847513c76cf180f24da21
Ben Ramchandani <Ben.Ramchandani@softwire.com>
2021-08-06 13:49:45 +0100

    Fix image resizing
    Use sqrt to stop making images giant

--- order-processing-app/processing/process_image.py
+++ order-processing-app/processing/process_image.py
@@ -1,14 +1,16 @@
 from typing import Tuple
+from utils.timeit import time_it
 from processing.canny_edge_detector import CannyEdgeDetector
 from PIL import Image
 import numpy as np
+import math
 
-edge_detector = CannyEdgeDetector(lowthreshold=0.04, highthreshold=0.13)
-TARGET_SIZE_PX = 500_000
+TARGET_SIZE_PX = 1_000_000
 
 def process_image(pic: Image.Image) -> Tuple[int, Image.Image]:
     pixels = np.array(_normalise_size(pic))
-    
+
+    edge_detector = CannyEdgeDetector(lowthreshold=0.04, highthreshold=0.13)
     edges = edge_detector.detect(pixels)
     edginess = _calculate_edginess(edges)
 
@@ -16,9 +18,10 @@ def process_image(pic: Image.Image) -> Tuple[int, Image.Image]:
 
     return (edginess, result_img)
 
+@time_it
 def _normalise_size(pic: Image.Image) -> Image.Image:
     size = pic.width * pic.height
-    scale_factor = TARGET_SIZE_PX / size
+    scale_factor = math.sqrt(TARGET_SIZE_PX / size)
     return pic.resize((int(pic.width * scale_factor), int(pic.height * scale_factor)), Image.LANCZOS)
 
 def _calculate_edginess(edge_pixels: np.ndarray):
